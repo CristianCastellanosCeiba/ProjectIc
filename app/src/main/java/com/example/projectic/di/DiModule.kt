596d923003e32758e4f1c6af35bcdcc5cba3ce96
Cristian.Castellanos <cristian.castellanos@ceiba.com.co>
2021-11-25 06:41:54 -0500

    Fix gradle, change version implementation

--- app/src/main/java/com/example/projectic/di/DiModule.kt
+++ app/src/main/java/com/example/projectic/di/DiModule.kt
@@ -1,14 +1,5 @@
 package com.example.projectic.di
 
-import com.example.domain.repository.AutoRepository
-import com.example.domain.repository.MotorcycleRepository
-import com.example.infrastructure.repository.AutoRepositoryImpl
-import com.example.infrastructure.repository.MotorcycleRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-
 //@Module
 //@InstallIn(ActivityComponent::class)
 abstract class DiModule {
